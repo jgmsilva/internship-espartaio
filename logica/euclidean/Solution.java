b043525c9a1dcfb5c9ecaa30f834c5167f9e6785
jgmsilva <58925924+jgmsilva@users.noreply.github.com>
2021-09-06 17:45:39 -0300

    euclidean finished without comments

--- logica/euclidean/Solution.java
+++ logica/euclidean/Solution.java
@@ -1,5 +1,15 @@
 public class Solution {
   public static int solution(int N, int M){
-
+    int[] eaten = new int[N];
+    int chosen = 0;
+    int i = 0;
+    for (; i < N; i++) {
+      if (eaten[chosen] == 1) {
+        return i;
+      }
+      eaten[chosen] = 1;
+      chosen = (chosen + M) % N;
     }
+    return i;
   }
+}
