ef089c7aba0b5678e6e6829dab21825000358f44
jgmsilva <58925924+jgmsilva@users.noreply.github.com>
2021-09-06 17:26:09 -0300

    distinct finished without comments

--- logica/distinct/Solution.java
+++ logica/distinct/Solution.java
@@ -1,7 +1,24 @@
 public class Solution{
-   public int solution(int[] A){
-     
+   public static int solution(int[] A){
+     int[] distincts = new int[A.length];
+     int length = 0;
+     for (int i=0; i < A.length ; i++) {
+       if (!in(distincts, A[i])) {
+         length = insert(distincts, length, A[i]);
+      }
+     }
+     return distincts.length;
    }
-
-
-}
+   private static boolean in(int[] vector, int value){
+      for (int v : vector) {
+        if (v == value) {
+          return true;
+        }
+      }
+      return false;
+   }
+   private static int insert(int[] vector, int length, int value){
+     vector[length] = value;
+     return length+1;
+   }
+ }
