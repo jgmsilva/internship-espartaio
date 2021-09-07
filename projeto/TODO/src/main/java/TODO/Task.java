8633dbcdb76aeb7378a7b94c3fc0cb6702a75a1c
jgmsilva <58925924+jgmsilva@users.noreply.github.com>
2021-09-07 20:40:07 -0300

    added status

--- projeto/TODO/src/main/java/TODO/Task.java
+++ projeto/TODO/src/main/java/TODO/Task.java
@@ -4,10 +4,12 @@ package TODO;
 
 public class Task {
     private String description;
+    private boolean finished;
     private boolean selected;
     
     public Task(String description){
         this.description = description;
+        this.finished = false;
         this.selected = false;
     }
     
@@ -15,8 +17,20 @@ public class Task {
         this.selected = check;
     }
     
+    public void setFinished(boolean check){
+        this.finished = check;
+    }
+    
     public String getDescription(){
         return description;
+    }
+    
+    public String getFinished(){
+        if(finished){
+            return "Finalizada";
+        }else{
+            return "Pendente";
+        }
     }
     
     public boolean isSelected(){
