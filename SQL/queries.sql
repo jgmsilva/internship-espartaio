cfb2744db124cdfd2a48b5e93d1f201526ddd2eb
jgmsilva <58925924+jgmsilva@users.noreply.github.com>
2021-09-06 23:08:13 -0300

    all queries redone

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,19 +1,36 @@
+-- Buscar os nomes de todos os alunos que frequentam alguma turma do professor 'JOAO PEDRO'.
 SELECT Nome
-  FROM ALUNO JOIN ALUNO_TURMA JOIN TURMA ON (ALUNO.id=ALUNO_TURMA.aluno_id AND ALUNO_TURMA.turma_id=TURMA.id)
-  WHERE PROFESSOR_id IN (SELECT id FROM PROFESSOR WHERE nome = 'JOAO PEDRO')
+  FROM ALUNO
+  WHERE id IN (SELECT aluno_id
+                FROM ALUNO_TURMA JOIN TURMA JOIN PROFESSOR ON (ALUNO_TURMA.turma_id=TURMA.id AND TURMA.PROFESSOR_id=PROFESSOR.id)
+                WHERE PROFESSOR.nome = 'JOAO PEDRO')
 
+-- Buscar os dias da semana que tenham aulas da disciplina 'MATEMATICA'.
 SELECT DISTINCT dia_da_semana
-  FROM TURMA
-  WHERE DISCIPLINA_id IN (SELECT id FROM DISCIPLINA WHERE nome = 'MATEMATICA')
+  FROM TURMA JOIN DISCIPLINA ON TURMA.DISCIPLINA_id=DISCIPLINA.id
+  WHERE DISCIPLINA.nome = 'MATEMATICA'
 
+-- Buscar todos os alunos que frequentem aulas de 'MATEMATICA' e também 'FISICA'.
 SELECT Nome
-  FROM ALUNO JOIN ALUNO_TURMA JOIN TURMA ON (ALUNO.id=ALUNO_TURMA.aluno_id AND ALUNO_TURMA.turma_id=TURMA.id)
-  WHERE DISCIPLINA_id IN (SELECT id FROM DISCIPLINA WHERE nome = 'MATEMATICA' AND nome = 'FISICA')
+  FROM ALUNO
+  WHERE id IN (SELECT aluno_id
+                FROM ALUNO_TURMA JOIN TURMA JOIN DISCIPLINA ON (ALUNO_TURMA.turma_id=TURMA.id AND TURMA.DISCIPLINA_id=DISCIPLINA.id)
+                 WHERE nome = 'MATEMATICA')
+        AND IN (SELECT aluno_id
+                      FROM ALUNO_TURMA JOIN TURMA JOIN DISCIPLINA ON (ALUNO_TURMA.turma_id=TURMA.id AND TURMA.DISCIPLINA_id=DISCIPLINA.id)
+                       WHERE nome = 'FISICA')
 
+-- Buscar as disciplinas que não tenham nenhuma turma.
 SELECT nome
   FROM DISCIPLINA
   WHERE id NOT IN (SELECT DISCIPLINA_id FROM TURMA)
 
+-- Buscar os alunos que frequentem aulas de 'MATEMATICA' exceto os que frequentem 'QUIMICA'.
 SELECT Nome
-  FROM ALUNO JOIN ALUNO_TURMA JOIN TURMA ON (ALUNO.id=ALUNO_TURMA.aluno_id AND ALUNO_TURMA.turma_id=TURMA.id)
-  WHERE DISCIPLINA_id IN (SELECT id FROM DISCIPLINA WHERE nome = 'MATEMATICA') AND NOT IN ()
+  FROM ALUNO
+  WHERE id IN (SELECT aluno_id
+                FROM ALUNO_TURMA JOIN TURMA JOIN DISCIPLINA ON (ALUNO_TURMA.turma_id=TURMA.id AND TURMA.DISCIPLINA_id=DISCIPLINA.id)
+                WHERE DISCIPLINA.nome = 'MATEMATICA')
+        AND NOT IN (SELECT aluno_id
+                    FROM ALUNO_TURMA JOIN TURMA JOIN DISCIPLINA ON (ALUNO_TURMA.turma_id=TURMA.id AND TURMA.DISCIPLINA_id=DISCIPLINA.id)
+                    WHERE DISCIPLINA.nome = 'QUIMICA')
