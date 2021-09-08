ecad90311ce2e3719ce7fbecd56f632cc7c5aaea
jgmsilva <58925924+jgmsilva@users.noreply.github.com>
2021-09-08 11:32:39 -0300

    clarify iterator initialization

--- logica/euclidean/Solution.java
+++ logica/euclidean/Solution.java
@@ -2,7 +2,7 @@ public class Solution {
   public static int solution(int N, int M){
     int[] eaten = new int[N];
     int chosen = 0;
-    int i = 0;
+    int i = 0; // i inicializado aqui para poder ser retornado fora do loop
     for (; i < N; i++) {
       if (eaten[chosen] == 1) {
         return i;
